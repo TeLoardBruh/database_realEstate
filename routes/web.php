9c50004a7ffbdebfe21cbfc5ac3bdebab4958414
Rax <damnloardbruh@gmail.com>
2020-01-13 16:18:41 +0700

    done auth both admin and agent

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+Route::view('/', 'welcome');
+Auth::routes();
+
+Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm')->name('login.admin');
+Route::get('/login/agent', 'Auth\LoginController@showAgentLoginForm')->name('login.agent');
+Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm')->name('register.admin');
+Route::get('/register/agent', 'Auth\RegisterController@showAgentRegisterForm')->name('register.agent');
+
+Route::post('/login/admin', 'Auth\LoginController@adminLogin');
+Route::post('/login/agent', 'Auth\LoginController@agentLogin');
+Route::post('/register/admin', 'Auth\RegisterController@createAdmin')->name('register.admin');
+Route::post('/register/agent', 'Auth\RegisterController@createAgent')->name('register.agent');
+
+Route::view('/home', 'home')->middleware('auth');
+Route::group(['middleware' => 'auth:admin'], function () {
+    Route::view('/admin', 'admin');
+});
+
+Route::group(['middleware' => 'auth:agent'], function () {
+    Route::view('/agent', 'agent');
+});
